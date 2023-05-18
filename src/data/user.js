ca95d6b989783e3fce82a8c73b4fde08898aba15
PedroTresmondi <pedro_tresmondi@hotmail.com>
2023-05-18 20:26:54 -0300

    atualizando link web befit

--- src/data/user.js
+++ src/data/user.js
@@ -34,7 +34,7 @@ const INFO = {
 			description:"Projeto feito em grupo na faculdade. Com o Befit, os usuários podem criar treinos personalizados na versão web do aplicativo, selecionando uma variedade de exercícios disponíveis. ",
 			logo: "../logo-befit-azul.png",
 			linkText: "Projeto no GitHub",
-			link:"https://github.com/SPTechBeFit/BeFit/tree/develop",
+			link:"https://befit-6b9r34j7u-befit.vercel.app/",
 		},
 	],
 	contact: [
