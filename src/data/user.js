d97e334d4e4b428550c75988edc14f36c5d9f6a3
PedroTresmondi <pedro_tresmondi@hotmail.com>
2023-05-18 18:13:42 -0300

    mudando texto na home

--- src/data/user.js
+++ src/data/user.js
@@ -18,7 +18,7 @@ const INFO = {
 	},//Possuo habilidades em JavaScript, Node.js, React.JS, AWS e UI/UX. Desenvolvo soluções de software inovadoras, incluindo aplicativos web eficientes e soluções em nuvem confiáveis. Estou preparado para projetos desafiadores e inovadores durante minha jornada de trabalho.
 
 	about: {
-		title: "Moro em São Paulo, procuro evoluir cada vez mais.",
+		title: "Moro em São Paulo, procuro evoluir cada vez mais📈.",
 		description:
 		"Um engenheiro de software habilidades em JavaScript, Node.js, React.JS e AWS. Trabalho em projetos internos, aplicando metodologias ágeis como Scrum e desenvolvendo soluções inovadoras. Também sou designer gráfico freelancer com experiência em Adobe Illustrator e Photoshop. Atualmente, estou cursando Análise de Sistemas de Computação na São Paulo Tech School, adquirindo conhecimentos em diversas tecnologias como HTML, Java, JavaScript, SQL, Bootstrap, React, AWS e Azure. Estou animado para aplicar minhas habilidades em projetos desafiadores e inovadores."},
 	projects: [
